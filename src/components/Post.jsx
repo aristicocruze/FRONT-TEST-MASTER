281f6d8dd1a6c63a5252024971ac597b4f2759b9
Aristides Cruz <aristidescruz02@gmail.com>
2022-03-22 11:58:08 +0100

    Display images data in Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,11 @@
-export default function Post() {
-  return <div>Post</div>;
+export default function Post({ post }) {
+  return (
+    <div style={{ margin: "200px" }}>
+      <h2>{`Autor: ${post.author}`}</h2>
+      <h2>{`id: ${post.id}`}</h2>
+      <h2>{`liked: ${post.liked}`}</h2>
+      <h2>{`Likes: ${post.likes_count}`}</h2>
+      <h2>{`Price: ${post.price}`}</h2>
+    </div>
+  );
 }
