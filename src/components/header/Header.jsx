beb555d0e14dc79d9eb3ac4412e20b97e37ea8d2
Aristides Cruz <aristidescruz02@gmail.com>
2022-03-28 07:02:17 +0200

    Add search icon in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
 import styles from "./header.module.css";
+import { BiSearch } from "react-icons/bi";
 
 export default function Header({ handleSearch }) {
   return (
     <div className={styles.header}>
-      <input
-        type="text"
-        placeholder="You're looking for something?"
-        onChange={handleSearch}
-        className={styles.search}
-      />
+      <div className={styles.searchContainer}>
+        <BiSearch className={styles.searchIcon} />
+        <input
+          type="text"
+          placeholder="You're looking for something?"
+          onChange={handleSearch}
+          className={styles.searchInput}
+        />
+      </div>
     </div>
   );
 }
